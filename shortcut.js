36f82d9c0826cfed1879d487535e8b7bb913bf96
Prateek K. Keshari <prateekkeshari7@gmail.com>
2023-06-12 00:00:36 +0200

    deleted shortcuts

--- shortcut.js
+++
@@ -1,6 +0,0 @@
-document.getElementById('shortcutForm').addEventListener('submit', function(e) {
-    e.preventDefault();
-    const shortcut = document.getElementById('shortcut').value;
-    window.myIpcRenderer.send('set_shortcut', shortcut);
-  });
-  
